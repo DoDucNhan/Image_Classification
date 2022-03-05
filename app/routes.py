a7c7d9d31814d29817163dd31afb7a2e92b53ade
DoDucNhan <doducnhan1999@gmail.com>
2022-03-05 11:13:09 +1030

    english version

--- app/routes.py
+++ app/routes.py
@@ -5,12 +5,12 @@ from app import app
 @app.route('/', methods=['GET'])
 @app.route('/flowers', methods=['GET'])
 def login():
-    return render_template('flowers.html', title='Flowers', header='HOA', status1="active")
+    return render_template('flowers.html', title='Flowers', header='FLOWERS', status1="active")
 
 
 @app.route('/cats_dogs', methods=['GET'])
 def cats_dogs():
-    return render_template('cats_dogs.html', title='Cats vs dogs', header='CHÓ MÈO', status2="active")
+    return render_template('cats_dogs.html', title='Cat vs dog', header='CAT AND DOG', status2="active")
 
 
 
